845d5f13984f727f2fef01516ea20e955331b523
wacko <103905555+baileyfuhad@users.noreply.github.com>
2026-04-20 15:54:10 -0400

    Add Reports class with monthly and division report methods

---
+++ src/Reports.java
@@ -0,0 +1,10 @@
+public class Reports {
+
+    public void monthlyPayReport() {
+        System.out.println("Monthly Pay Report");
+    }
+
+    public void divisionReport() {
+        System.out.println("Division Report");
+    }
+}
