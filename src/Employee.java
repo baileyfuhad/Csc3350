db2fe577082581b35ffbc5deea82b1fa719a3c63
wacko <103905555+baileyfuhad@users.noreply.github.com>
2026-04-20 15:52:34 -0400

    Add Employee class with constructor

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    int empId;
+    String name;
+    double salary;
+
+    public Employee(int empId, String name, double salary) {
+        this.empId = empId;
+        this.name = name;
+        this.salary = salary;
+    }
+}
