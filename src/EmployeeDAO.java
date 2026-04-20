12040c171b300242d542cffdf17bce859a41d9e1
wacko <103905555+baileyfuhad@users.noreply.github.com>
2026-04-20 15:53:46 -0400

    Create EmployeeDAO class with CRUD methods

---
+++ src/EmployeeDAO.java
@@ -0,0 +1,18 @@
+public class EmployeeDAO {
+
+    public void addEmployee() {
+        System.out.println("Employee Added");
+    }
+
+    public void searchEmployee() {
+        System.out.println("Employee Found");
+    }
+
+    public void updateEmployee() {
+        System.out.println("Employee Updated");
+    }
+
+    public void deleteEmployee() {
+        System.out.println("Employee Deleted");
+    }
+}
