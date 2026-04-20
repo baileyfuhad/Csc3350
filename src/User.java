1412db16e8bb0ca2e962acaaba45930c87694857
wacko <103905555+baileyfuhad@users.noreply.github.com>
2026-04-20 16:01:28 -0400

    Add User class with username and role attributes

---
+++ src/User.java
@@ -0,0 +1,9 @@
+public class User {
+    String username;
+    String role;
+
+    public User(String username, String role) {
+        this.username = username;
+        this.role = role;
+    }
+}
