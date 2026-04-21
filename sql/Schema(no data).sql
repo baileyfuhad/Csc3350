9de45deaf462f3377010f55248c27456de145d45
Joshi-64 <joshlif69@gmail.com>
2026-04-21 14:17:02 -0400

    new file:   sql/Populate_tables.sql 	new file:   sql/Schema(no data).sql 	deleted:    sql/schema.sql

---
+++ sql/Schema(no data).sql
@@ -0,0 +1,196 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: employeedata
+-- ------------------------------------------------------
+-- Server version	9.5.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '8d97e609-074b-11f1-8b5c-00155d1ad29a:1-220';
+
+--
+-- Table structure for table `addresses`
+--
+
+DROP TABLE IF EXISTS `addresses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `addresses` (
+  `street` varchar(100) NOT NULL,
+  `cityID` int NOT NULL,
+  `StateID` int NOT NULL,
+  `zip` varchar(10) NOT NULL,
+  `DOB` date DEFAULT NULL,
+  `Phone_num` varchar(15) DEFAULT NULL,
+  `Emergency_name` varchar(100) DEFAULT NULL,
+  `Emergency_phone` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cities`
+--
+
+DROP TABLE IF EXISTS `cities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cities` (
+  `cityID` int NOT NULL,
+  `city_name` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `division`
+--
+
+DROP TABLE IF EXISTS `division`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `division` (
+  `div_ID` int NOT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  `city` varchar(50) NOT NULL,
+  `addressLine1` varchar(50) NOT NULL,
+  `addressLine2` varchar(50) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `country` varchar(50) NOT NULL,
+  `postalCode` varchar(15) NOT NULL,
+  PRIMARY KEY (`div_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='company divisions';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `employee_division`
+--
+
+DROP TABLE IF EXISTS `employee_division`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee_division` (
+  `empid` int NOT NULL,
+  `div_ID` int NOT NULL,
+  PRIMARY KEY (`empid`),
+  KEY `employee_division_division_FK` (`div_ID`),
+  CONSTRAINT `employee_division_division_FK` FOREIGN KEY (`div_ID`) REFERENCES `division` (`div_ID`),
+  CONSTRAINT `employee_division_employees_FK` FOREIGN KEY (`empid`) REFERENCES `employees` (`empid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='links employee to a division';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `employee_job_titles`
+--
+
+DROP TABLE IF EXISTS `employee_job_titles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee_job_titles` (
+  `empid` int NOT NULL,
+  `job_title_id` int NOT NULL,
+  KEY `employee_job_titles_employees_FK` (`empid`),
+  KEY `employee_job_titles_job_titles_FK` (`job_title_id`),
+  CONSTRAINT `employee_job_titles_employees_FK` FOREIGN KEY (`empid`) REFERENCES `employees` (`empid`),
+  CONSTRAINT `employee_job_titles_job_titles_FK` FOREIGN KEY (`job_title_id`) REFERENCES `job_titles` (`job_title_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employees` (
+  `empid` int NOT NULL,
+  `Fname` varchar(65) NOT NULL,
+  `Lname` varchar(65) NOT NULL,
+  `email` varchar(65) NOT NULL,
+  `HireDate` date DEFAULT NULL,
+  `Salary` decimal(10,2) NOT NULL,
+  `SSN` varchar(12) DEFAULT NULL,
+  `addressID` int NOT NULL,
+  PRIMARY KEY (`empid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `job_titles`
+--
+
+DROP TABLE IF EXISTS `job_titles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `job_titles` (
+  `job_title_id` int NOT NULL,
+  `job_title` varchar(125) NOT NULL,
+  PRIMARY KEY (`job_title_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `payroll`
+--
+
+DROP TABLE IF EXISTS `payroll`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payroll` (
+  `payID` int DEFAULT NULL,
+  `pay_date` date DEFAULT NULL,
+  `earnings` decimal(8,2) DEFAULT NULL,
+  `fed_tax` decimal(7,2) DEFAULT NULL,
+  `fed_med` decimal(7,2) DEFAULT NULL,
+  `fed_SS` decimal(7,2) DEFAULT NULL,
+  `state_tax` decimal(7,2) DEFAULT NULL,
+  `retire_401k` decimal(7,2) DEFAULT NULL,
+  `health_care` decimal(7,2) DEFAULT NULL,
+  `empid` int DEFAULT NULL,
+  KEY `payroll_employees_FK` (`empid`),
+  CONSTRAINT `payroll_employees_FK` FOREIGN KEY (`empid`) REFERENCES `employees` (`empid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `states`
+--
+
+DROP TABLE IF EXISTS `states`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `states` (
+  `StateID` int NOT NULL,
+  `state_abbr` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'employeedata'
+--
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-21 14:14:08
