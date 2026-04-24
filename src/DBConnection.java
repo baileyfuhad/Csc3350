05af65b291deaa4024aa561825adb37b782406eb
kogonas <krumph3@student.gsu.edu>
2026-04-24 03:17:46 -0400

    Updated DBConnection.java to support DAO connections

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,5 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
-    public void connect() {
-        System.out.println("Connected to MySQL");
+
+    private static final String URL = "jdbc:mysql://YOUR_HOST:3306/YOUR_DATABASE";
+    private static final String USER = "YOUR_USERNAME";
+    private static final String PASSWORD = "YOUR_PASSWORD";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
