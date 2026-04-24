16e3e243d64e397d8c4c7b07c59fedf199357e0a
kogonas <krumph3@student.gsu.edu>
2026-04-24 03:08:47 -0400

    Updated Main.java to include Payroll and Reports menus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("CSC3350 Project Started");
+
+        Scanner sc = new Scanner(System.in);
+        PayrollUI payrollUI = new PayrollUI();
+        ReportsUI reportsUI = new ReportsUI();
+
+        while (true) {
+            System.out.println("=== MAIN MENU ===");
+            System.out.println("1. Payroll");
+            System.out.println("2. Reports");
+            System.out.println("0. Exit");
+            System.out.print("Choose: ");
+
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1 -> payrollUI.showPayrollMenu();
+                case 2 -> reportsUI.showReportsMenu();
+                case 0 -> {
+                    System.out.println("Goodbye!");
+                    return;
+                }
+            }
+        }
     }
 }
